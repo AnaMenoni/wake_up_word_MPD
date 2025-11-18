a35dab4d68a9dda4a6e9c8d5b2448e02b54cec86
AnaMenoni <anamenonisilva@gmail.com>
2025-11-18 19:30:20 -0300

    agrego funcionalidad para hacer graficas

--- src/detect.py
+++ src/detect.py
@@ -8,6 +8,8 @@ import joblib
 import numpy as np
 from src.preprocess import preprocess_wav
 from src.features import compute_mel_spectrogram
+import matplotlib.pyplot as plt
+import librosa.display
 
 
 def detect_wakeword(filepath, model_path="modelo.pkl", scaler_path="scaler.pkl"):
@@ -21,6 +23,11 @@ def detect_wakeword(filepath, model_path="modelo.pkl", scaler_path="scaler.pkl")
     x = preprocess_wav(filepath)
 
     mel = compute_mel_spectrogram(x)
+
+    graficar_senal(x)
+    graficar_stft(x)
+    graficar_mel(mel)
+    graficar_features(mel)
 
     mel_mean = mel.mean(axis=1)   # promedio por cada banda Mel
     mel_std  = mel.std(axis=1)    # desviación estándar por banda Mel
@@ -36,3 +43,51 @@ def detect_wakeword(filepath, model_path="modelo.pkl", scaler_path="scaler.pkl")
         print("Wake-word detectada.")
     else:
         print("No detectada.")
+
+def graficar_senal(x, sr=16000):
+    plt.figure(figsize=(12,3))
+    plt.title("Señal temporal preprocesada")
+    plt.plot(x)
+    plt.xlabel("Muestras")
+    plt.ylabel("Amplitud")
+    plt.tight_layout()
+    plt.show()
+
+
+def graficar_stft(x, sr=16000):
+    X = librosa.stft(x)
+    X_db = librosa.amplitude_to_db(abs(X))
+
+    plt.figure(figsize=(12,4))
+    librosa.display.specshow(X_db, sr=sr, x_axis='time', y_axis='hz', cmap='magma')
+    plt.colorbar(format="%+2.f dB")
+    plt.title("Espectrograma STFT")
+    plt.tight_layout()
+    plt.show()
+
+
+def graficar_mel(mel, sr=16000):
+    mel_db = librosa.power_to_db(mel, ref=np.max)
+
+    plt.figure(figsize=(12,4))
+    librosa.display.specshow(mel_db, sr=sr, x_axis='time', y_axis='mel', cmap='magma')
+    plt.colorbar(format="%+2.f dB")
+    plt.title("Mel-Espectrograma")
+    plt.tight_layout()
+    plt.show()
+
+def graficar_features(mel):
+    mel_mean = mel.mean(axis=1)
+    mel_std  = mel.std(axis=1)
+
+    plt.figure(figsize=(12,4))
+    plt.plot(mel_mean, label="mean")
+    plt.plot(mel_std, label="std")
+    plt.title("Características Mel (mean / std)")
+    plt.xlabel("Banda Mel")
+    plt.ylabel("Valor")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
